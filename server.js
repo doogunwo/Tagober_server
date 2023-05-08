500ba59dd26c13ce45d964e9e46820168485fcb2
doogunwo <dgw0601@naver.com>
2023-05-09 05:04:26 +0900

    회원등록및가입만드는중

--- server.js
+++ server.js
@@ -3,25 +3,61 @@ const multer = require('multer');
 const axios = require('axios');
 const app = express();
 const upload = multer();
-
-app.post('/', upload.single('image'), async (req, res) => {
-  const formData = new FormData();
-  formData.append('image', req.file.buffer, {
-    filename: req.file.originalname,
-  });
-
-  try {
-    const response = await axios.post('http://192.168.35.135/add_member', formData, {
-      headers: formData.getHeaders(),
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Failed to upload image.' });
-  }
+const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // for parsing application/json
+const connection = mysql.createConnection({
+  host: 'ans3.cwmxwotwq2p1.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'ehrjsdn123!',
+  database: 'sys'
 });
 
-//클라이언트에서 새로운 회원가입이 오면 
+connection.connect((err) => {
+  if (err) {
+    console.error('MySQL connection failed: ', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Page', 'Main.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Page', 'login.html'));
+});
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Page', 'signup.html'));
+});
+
+
+app.post('/signup', (req, res) => {
+  const { username, password, email, phone, name } = req.body;
+  console.log(req.body);
+  // 회원 정보를 MySQL에 저장
+  const query = `INSERT INTO signup (username, password, email, phone, name) VALUES (?, ?, ?, ?, ?)`;
+  const values = [username, password, email, phone, name];
+
+  connection.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error saving signup data to MySQL: ', err);
+      res.status(500).send('Error saving signup data');
+      return;
+    }
+
+    console.log('Signup data saved to MySQL');
+    res.status(200).send('Signup successful');
+  });
+});
+
+
+/*
+  //클라이언트에서 새로운 회원가입이 오면 
 const url = 'http://192.168.35.135:5000/add_member';
 const data = {
   key1: '이름',
@@ -37,9 +73,8 @@ axios.post(url, data)
     console.error(error);
   });
 
-
-
-
-  app.listen(3000, () => {
+*/
+app.use(express.static('public'));
+app.listen(3000, () => {
     console.log('Server is listening on port 3000!');
   });
