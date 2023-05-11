ab6aac626eea005a287f20a235edd9bc9203bc3b
doogunwo <dgw0601@naver.com>
2023-05-12 02:08:51 +0900

    세션 등록

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const app = express();
 const upload = multer();
 const path = require('path');
 const mysql = require('mysql');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { Console } = require('console');
+const { userInfo } = require('os');
 app.use(bodyParser.json()) // for parsing application/json
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+const session = require('express-session');
+
+app.use(
+  session({
+    secret: 'your-secret-key', // 세션 암호화를 위한 비밀 키
+    resave: false, // 변경이 없더라도 세션을 항상 저장할지 여부
+    saveUninitialized: false, // 초기화되지 않은 세션을 저장할지 여부
+    // 추가적인 옵션 설정 가능 (예: 쿠키 설정, 세션 저장소 등)
+  })
+);
 
 const connection = mysql.createConnection({
   host: 'ans3.cwmxwotwq2p1.us-east-1.rds.amazonaws.com',
@@ -35,9 +51,17 @@ app.get('/login', (req, res) => {
 app.get('/signup', (req, res) => {
   res.sendFile(path.join(__dirname, 'Page', 'signup.html'));
 });
+
 app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Page', 'dashboard.html'));
+   // 쿠키 설정
+    res.sendFile(path.join(__dirname, 'Page', 'dashboard.html'));
 });
+
+app.get('/getUsername', (req, res) => {
+  const username = req.session.username; // 세션에서 회원 이름 가져오기
+  res.json({ username }); // 회원 이름을 JSON 형식으로 응답
+});
+
 
 app.post('/signup', (req, res) => {
   // 클라이언트로부터 전송된 데이터 수신
@@ -51,35 +75,36 @@ app.post('/signup', (req, res) => {
     } else {
       console.log('MySQL 저장 성공');
       res.json({ message: '회원가입이 완료되었습니다.' });
+      
     }
   });
 });
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  
-
+  req.session.username = username;
   // MySQL 데이터베이스에서 사용자 정보 검증
   // 이 예시에서는 간단한 비교를 통해 로그인 검증을 수행합니다.
   // 실제로는 bcrypt나 passport 등을 사용하여 안전한 방식으로 비밀번호를 검증해야 합니다.
-
   const query = `SELECT * FROM signup WHERE username = '${username}'`;
-  console.log(query)
-  
-
   connection.query(query, (err, results) => {
     if (err) {
       console.error('MySQL query error: ', err);
       res.status(500).json({ message: 'Internal Server Error' });
       return;
     }
-
+    
     // 사용자 정보가 존재하는 경우
     if (results.length > 0) {
       const user = results[0];
-
+      console.log(user)
+      
+      
       // 비밀번호가 일치하는 경우
       if (user.password === password) {
-        res.json({ message: 'Login successful' });
+        console.log("username",user.name);
+        
+        res.sendStatus(200);
+       
       } else {
         res.status(401).json({ message: 'Invalid password' });
       }
